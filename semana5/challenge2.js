9ecd91f2994cea2c55a2d9ba43d40c1a7a8a182a
jesaguilarhub <aguilar.codv@gmail.com>
2021-09-21 13:32:19 -0500

    Soluciones a la semana 5

--- semana5/challenge2.js
+++ semana5/challenge2.js
@@ -1,6 +1,41 @@
 function plantTrees(w, l, g) {
-    //Implementación
-    return 0;
+	//Implementación
+	const CENTER = (w - 2) * 2;
+	let end = w * l - CENTER;
+	if (g === 0) return end;
+
+	let count = 1;
+	let start = 2;
+	let blank = 1;
+	g++;
+	while (start <= end) {
+		if (blank % g === 0) {
+			count++;
+			blank = 1;
+		}
+		else {
+			blank++;
+		}
+		start++;
+	}
+	if (blank > 2) return 0;
+	return count;
+	/* 
+
+        o o o o
+        o     o
+        o     o
+        o o o o
+        
+        o - o -
+        -     o
+        o     -
+        - o - o
+
+        o - o
+        -   -
+        o - o
+    */
 }
 
 module.exports = plantTrees;
